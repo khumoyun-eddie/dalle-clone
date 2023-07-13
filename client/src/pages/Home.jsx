029707cb64dc4488d4cf0096d4f30e84005bc2f0
khumoyun-eddie <eddie.freelance21@gmail.com>
2023-07-14 03:08:18 +0900

    server url changed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post");
+        const response = await fetch("https://dall-e-0ewg.onrender.com/api/v1/post");
         if (response.ok) {
           const result = await response.json();
           setAllPosts(result.data.reverse());
